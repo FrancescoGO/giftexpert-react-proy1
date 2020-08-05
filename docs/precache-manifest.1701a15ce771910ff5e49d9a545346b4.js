964ce8087c305276b7a9e6af64d2bb0595cd850e
FrancescoGO <francesco.go.os@gmail.com>
2020-08-04 21:17:52 -0500

    Docs agregado

---
+++ docs/precache-manifest.1701a15ce771910ff5e49d9a545346b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65d9d576a1475b6e90a0f63f482a1d5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cab55ee7a11d1afc7dda",
+    "url": "/static/css/main.33c31c06.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cab55ee7a11d1afc7dda",
+    "url": "/static/js/main.1ba0b2b9.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
